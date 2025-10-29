0ea066f2bd49a694cb88617f8fb9ededb31876f8
Lakshan N <143219317+Beer34@users.noreply.github.com>
2025-10-29 16:56:53 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,15 +1,8 @@
 from flask import Flask, jsonify
 import mysql.connector
-import os
+from config import get_db_config
 
 app = Flask(__name__)
-
-db_config = {
-    'host': os.getenv('DB_HOST', 'mysql_db'),
-    'user': os.getenv('DB_USER', 'user'),
-    'password': os.getenv('DB_PASSWORD', 'password'),
-    'database': os.getenv('DB_NAME', 'flightdb')
-}
 
 @app.route('/')
 def home():
@@ -17,13 +10,15 @@ def home():
 
 @app.route('/flights')
 def get_flights():
+    db_config = get_db_config()
     conn = mysql.connector.connect(**db_config)
     cursor = conn.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM flights")
-    result = cursor.fetchall()
+    cursor.execute("SELECT * FROM flights;")
+    flights = cursor.fetchall()
     cursor.close()
     conn.close()
-    return jsonify(result)
+    return jsonify(flights)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
